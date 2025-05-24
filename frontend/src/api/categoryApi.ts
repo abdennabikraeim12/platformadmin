2893b68630a79d6eee0d3c2fdc891a1b02d68575
Abdennabi Kraeim <abdennabikraeim@gmail.com>
2025-05-24 02:58:27 +0200

    S

--- frontend/src/api/categoryApi.ts
+++
@@ -1,8 +0,0 @@
-import type { Category } from "../types/category";
-import { apiClient } from "./apiClient";
-
-export const fetchCategories = async (): Promise<Category[]> => {
-  const { data } = await apiClient.get<Category[]>("/categories");
-  return data;
-};
-
