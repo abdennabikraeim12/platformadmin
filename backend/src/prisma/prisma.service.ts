e792ec14133e302e6937d552735263fee3601b65
Abdennabi Kraeim <abdennabikraeim@gmail.com>
2025-05-24 03:47:00 +0200

    U

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,22 +1,25 @@
-// src/prisma/prisma.service.ts
 import { INestApplication, Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    super(
+    super({
+      log: ['query', 'info', 'warn', 'error'],
+    }
     );
   }
-
   async onModuleInit() {
-    await this.$connect();
-  }
-
+try {
+      await this.$connect();
+      console.log('Prisma connected successfully');
+    } catch (error) {
+      console.error('Prisma connection error:', error);
+      throw error;
+    }  }
   async onModuleDestroy() {
     await this.$disconnect();
   }
-
   async enableShutdownHooks(app: INestApplication) {
     process.on('beforeExit', async () => {
       await app.close();
