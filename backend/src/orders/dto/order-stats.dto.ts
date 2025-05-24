d2faf4ad1607858f0a63ecc69c7e7fb9f4bd894f
Abdennabi Kraeim <abdennabikraeim@gmail.com>
2025-05-24 03:54:26 +0200

    U

--- backend/src/orders/dto/order-stats.dto.ts
+++ backend/src/orders/dto/order-stats.dto.ts
@@ -1,12 +1,16 @@
-export class SalesStatsDto {
-  period: string;
-  totalSales: number;
-  orderCount: number;
-  topProducts: { productId: number; name: string; sales: number }[];
-}
 
 export class OrderStatsDto {
   daily: SalesStatsDto[];
   weekly: SalesStatsDto[];
   monthly: SalesStatsDto[];
+}
+export class SalesStatsDto{
+  period: string;
+  totalSales: number;
+  orderCount: number;
+  topProducts: {
+    productId: number;
+    name: string;
+    sales: number;
+  }[];
 }
