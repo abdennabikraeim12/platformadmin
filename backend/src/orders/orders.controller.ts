dbd7d50768dd99da5ee7042bb43a886656c71898
Abdennabi Kraeim <abdennabikraeim@gmail.com>
2025-05-24 03:55:24 +0200

    U

--- backend/src/orders/orders.controller.ts
+++ backend/src/orders/orders.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderResponseDto } from './dto/order-response.dto';
-import { OrderStatsDto } from './dto/order-stats.dto';
+import { OrderStatsDto, SalesStatsDto } from './dto/order-stats.dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -12,14 +12,23 @@ export class OrdersController {
   async create(@Body() createOrderDto: CreateOrderDto): Promise<OrderResponseDto> {
     return this.ordersService.createOrder(createOrderDto);
   }
+   @Get()
+    findAll(): Promise<OrderResponseDto[]> {
+      return this.ordersService.findAll();
+    }
+    @Get('monthly-sales')
+async getMonthlySales(@Query('year') year: string): Promise<SalesStatsDto[]> {
+  return this.ordersService.getMonthlySales(+year);
+}
+  @Get('stats')
+  async getStats(): Promise<OrderStatsDto> {
+    return this.ordersService.getOrderStats();
+  }
 
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<OrderResponseDto> {
     return this.ordersService.getOrderById(+id);
   }
 
-  @Get('stats')
-  async getStats(): Promise<OrderStatsDto> {
-    return this.ordersService.getOrderStats();
-  }
+  
 }
