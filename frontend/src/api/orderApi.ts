770dc35f7784980ee20d6c98ee09f1a9c06994ea
Abdennabi Kraeim <abdennabikraeim@gmail.com>
2025-05-24 03:05:26 +0200

    Delete frontend directory

--- frontend/src/api/orderApi.ts
+++
@@ -1,7 +0,0 @@
-import type { Order } from "../types/order";
-import { apiClient } from "./apiClient";
-
-export const fetchOrders = async (): Promise<Order[]> => {
-  const { data } = await apiClient.get<Order[]>("/orders");
-  return data;
-};
--- frontend/src/pages/ProductsPage/index.ts
+++
@@ -1,1 +0,0 @@
-export { ProductsPage as default } from './ProductsPage';
